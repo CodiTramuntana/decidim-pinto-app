984f28992a941e04d8d52212263578a350039651
Laura Jaime <laura.jv@coditramuntana.com>
2021-12-17 10:39:55 +0100

    Fix secret_key and homepage test

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -8,25 +8,28 @@ describe "Homepage", type: :system do
   let!(:organization) do
     create(
       :organization,
-      name: "Decidim Pinto",
+      name: "Decidim Application",
       default_locale: :es,
       available_locales: [:en, :es]
     )
   end
-  let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: { "welcome_text_es"=>"Bienvenido a Decidim Pinto" } }
+  let!(:hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: { "welcome_text_en"=>"Welcome to Decidim Application" } }
   let!(:sub_hero) { create :content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero }
 
   before do
+    I18n.locale = :en
     switch_to_host(organization.host)
     visit decidim.root_path
   end
 
   it "loads and shows organization name and main blocks" do
-    visit decidim.root_path
-
-    expect(page).to have_content("Decidim Pinto")
+    expect(page).to have_content("Decidim Application")
     within "section.hero .hero__container" do
-      expect(page).to have_content("Bienvenido a Decidim Pinto")
+      expect(page).to have_content("Welcome to Decidim Application")
+    end
+    within "section.subhero" do
+      subhero_msg= translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, "\\1")
+      expect(page).to have_content(subhero_msg)
     end
   end
 end
